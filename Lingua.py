cb10a6d14ed236216776999b862176c7a8f2a817
Miroshnichenko Dmitriy <2543810@gmail.com>
2014-07-14 17:16:07 +0400

    Change creds

--- Lingua.py
+++ Lingua.py
@@ -1,24 +1,21 @@
 __author__ = 'fish'
-import urllib2
+# -*- coding: utf-8 -*-
 import requests
+import sys
 
-word = 'help'
-mail = ''
-passwd = ''
+reload(sys)
+sys.setdefaultencoding("utf8")
+mail = '2543810@gmail.com'
+passwd = 'aC1IHR'
+r = ''
+cookie = {}
+w = ''
+words = (
+'absence', 'account', 'actuality', 'adjustment', 'administer', 'adversary', 'advertisement', 'agreement', 'amusement',
+'apparatus')
 
-link = 'http://api.lingualeo.com/gettranslates?word=' + word
 
-cookie = {'servid':'f3a14c0aa1828146590a5c0e5bcefe1a7741d68d51c12d25e6ba40da7918893ed2d7963628da7252',
-          "userid":"3299",
-          "AWSELB" :"75C701150A9420ACA77B49A59BB2636792D3E5911E677A65153DA70A80FDAAD4FB2493A73BAFD8779ADEAAAC382AFA7952EDE5B45F6C8A9C148E59341A1745F2BD0BEABDA2917467E67EE73E125B9EDCB7159DE56D",
-          "remember":"e30c0000af17f67875ab20ad59d1ed6aa5c8161a4876d31e25ee67f1996883d45e06b1058abdfa0d"}
-
-hit = requests.get(url=link, cookies=cookie)
-translation = ((hit.text).split(',')[7]).split(":")[1]
-picture = ((((hit.text).encode('ascii', 'replace')).split(',')[5]).split('"')[3]).replace('\\', '')
-sound = ((((hit.text).encode('ascii', 'replace')).split(',')[-1]).split('"')[3]).replace('\\', '')
-
-def save_file():
+def download_file():
     with open('/Users/fish/' + word + '.jpg', 'wb') as handle:
         response = requests.get(picture, stream=True)
         for block in response.iter_content(1024):
@@ -26,43 +23,51 @@ def save_file():
                 break
             handle.write(block)
 
-    r = requests.get(picture)
-    with open("picture.jpg", "wb") as code:
-    code.write(r.content)
 
-def get_sound(word):
-    url = "http://translate.google.com/translate_tts?tl=en&q=" + word
-    request = urllib2.Request(url)
-    request.add_header('User-agent', 'Mozilla/5.0')
-    opener = urllib2.build_opener()
+def make_word(so):
+    global w
+    word = ((so).split(',')[3]).split('"')[3]
+    transcription = (((so).split(',')[-4]).split(':')[1]).encode('utf-8')
+    translation = ((so).split(',')[7]).split(":")[1]
+    picture = ((((so).encode('ascii', 'replace')).split(',')[5]).split('"')[3]).replace('\\', '')
+    sound = ((((so).encode('ascii', 'replace')).split(',')[-1]).split('"')[3]).replace('\\', '')
+    return save_files(w, (transcription).split('"')[1], (translation).split('"')[1], (picture).split('/')[-1],
+                      (sound).split('/')[-1])
 
-    f = open(word + ".mp3", "wb")
-    f.write(opener.open(request).read())
-    f.close()
-    print "OK"
-    return 0
 
-def create_cookie(cookie):
-    test = cookie.cookies.extract_cookies.im_self.get('servid')
-    print test
+def save_files(word, transcription, translation, picture, sound):
+    with open('test1.txt', 'a') as handle:
+        handle.write(
+            '\n' + word + '\t' + transcription + '\t' + translation + '\t' + '<img src="' + picture + '">' + '\t' + '[sound:' + sound + ']')
 
 def login(email, password):
+    global r
+    global cookie
     url = 'http://lingualeo.ru/ru/login'
     data = {'email': email, 'password': password}
     r = requests.post(url=url, data=data)
-    cookie = create_cookie(r.cookies)
-    return cookie
+    cookie = {'servid': (r.cookies.extract_cookies.im_self.get('servid')),
+              "userid": "3299",
+              "AWSELB": (r.cookies.extract_cookies.im_self.get('AWSELB')),
+              "remember": "e30c0000af17f67875ab20ad59d1ed6aa5c8161a4876d31e25ee67f1996883d45e06b1058abdfa0d"}
+    return
+
+
+def ask_leo(word):
+    """
+
+    Делаем запрос в ЛингваЛео по заданому слову и возвращаем его значение в функцию make_word
+    """
+    global w
+    link = 'http://api.lingualeo.com/gettranslates?word=' + word
+    hit = requests.get(url=link, cookies=cookie)
+    w = word
+    return make_word(hit.text)
 
 def main():
-
-# cookie = login(mail, passwd)
-#print cookie
-    #print create_cookie(cookie)
-    #cookie = {'servid':'f3a14c0aa1828146590a5c0e5bcefe1a7741d68d51c12d25e6ba40da7918893ed2d7963628da7252',"userid" : "3299", "AWSELB" : "75C701150A9420ACA77B49A59BB2636792D3E5911E677A65153DA70A80FDAAD4FB2493A73BAFD8779ADEAAAC382AFA7952EDE5B45F6C8A9C148E59341A1745F2BD0BEABDA2917467E67EE73E125B9EDCB7159DE56D","remember" : "e30c0000af17f67875ab20ad59d1ed6aa5c8161a4876d31e25ee67f1996883d45e06b1058abdfa0d"}
-    #print requests.get(url='http://api.lingualeo.com/gettranslates?word=help',cookies=cookie).text
-
+    login(mail, passwd)
+    for word in words:
+        ask_leo(word)
 
 if __name__ == '__main__':
     main()
-
-
