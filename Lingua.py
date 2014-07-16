076d7ad11055f0a04b707405bbd1a6178df96cba
Miroshnichenko Dmitriy <2543810@gmail.com>
2014-07-16 16:53:40 +0400

    Almost done

--- Lingua.py
+++ Lingua.py
@@ -1,4 +1,4 @@
-__author__ = 'fish'
+__author__ = 'FishHead'
 # -*- coding: utf-8 -*-
 import requests
 import sys
@@ -6,16 +6,25 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf8")
 
-# Variables
+
 class Variables():
+    """
+    Variables collector :)
+    """
     def __init__(self):
-        self.mail = raw_input('Введите ваш email:\n')
-        self.passwd = raw_input('Введите ваш пароль:\n')
-        self.filename = raw_input('Введите название файла для выгрузки в Anki:\n')
+        self.mail = ''
+        self.password = ''
+        self.filename = ''
         self.r = ''
         self.cookie = {}
         self.w = ''
         self.words = ()
+
+
+def start():
+    Variables.mail = raw_input('Введите логин от сайта:\n')
+    Variables.password = raw_input('Введите пароль:\n')
+    Variables.filename = raw_input('Введите имя файла для выгрузки в Anki(включая расширение .txt):\n')
 
 
 def create_wordslist():
@@ -32,14 +41,12 @@ def login(email, password):
     """
     Login to LinguaLeo.
     """
-    global r
-    global cookie
     url = 'http://lingualeo.ru/ru/login'
     data = {'email': email, 'password': password}
-    r = requests.post(url=url, data=data)
-    cookie = {'servid': (r.cookies.extract_cookies.im_self.get('servid')),
+    Variables.r = requests.post(url=url, data=data)
+    Variables.cookie = {'servid': (Variables.r.cookies.extract_cookies.im_self.get('servid')),
               "userid": "3299",
-              "AWSELB": (r.cookies.extract_cookies.im_self.get('AWSELB')),
+              "AWSELB": (Variables.r.cookies.extract_cookies.im_self.get('AWSELB')),
               "remember": "e30c0000af17f67875ab20ad59d1ed6aa5c8161a4876d31e25ee67f1996883d45e06b1058abdfa0d"}
 
 
@@ -47,10 +54,9 @@ def ask_leo(word):
     """
     Do the query to LinguaLeo with chosen word and return value to function make_word.
     """
-    global w
     link = 'http://api.lingualeo.com/gettranslates?word=' + word
-    hit = requests.get(url=link, cookies=cookie)
-    w = word
+    hit = requests.get(url=link, cookies=Variables.cookie)
+    Variables.w = word
     return make_word(hit.text)
 
 
@@ -58,15 +64,13 @@ def make_word(so):
     """
     Getting definitions of the word.
     """
-    global w
-    # word = ((so).split(',')[3]).split('"')[3]
-    transcription = (((so).split(',')[-4]).split(':')[1]).encode('utf-8')
-    translation = ((so).split(',')[7]).split(":")[1]
-    picture = ((((so).encode('ascii', 'replace')).split(',')[5]).split('"')[3]).replace('\\', '')
-    sound = ((((so).encode('ascii', 'replace')).split(',')[-1]).split('"')[3]).replace('\\', '')
+    transcription = ((so.split(',')[-4]).split(':')[1]).encode('utf-8')
+    translation = (so.split(',')[7]).split(":")[1]
+    picture = (((so.encode('ascii', 'replace')).split(',')[5]).split('"')[3]).replace('\\', '')
+    sound = (((so.encode('ascii', 'replace')).split(',')[-1]).split('"')[3]).replace('\\', '')
     download_file(sound)
     download_file(picture)
-    return save_files(w, transcription.split('"')[1], translation.split('"')[1], picture.split('/')[-1],
+    return save_files(Variables.w, transcription.split('"')[1], translation.split('"')[1], picture.split('/')[-1],
                       sound.split('/')[-1])
 
 
@@ -74,16 +78,16 @@ def download_file(file):
     """
     Downloading image and audio files.
     """
-    r = requests.get(file)
+    Variables.r = requests.get(file)
     with open('C:\\Users\\Dmitriy\\Documents\\Anki\\fish\\collection.media\\' + file.split('/')[-1], "wb") as code:
-        code.write(r.content)
+        code.write(Variables.r.content)
 
 
 def save_files(word, transcription, translation, picture, sound):
     """
     Saving values into file for next import to Anki.
     """
-    with open('C:\\Users\\Dmitriy\\Documents\\Anki\\fish\\' + filename + '.txt', 'a') as handle:
+    with open('C:\\Users\\Dmitriy\\Documents\\Anki\\fish\\' + Variables.filename + '.txt', 'a') as handle:
         handle.write(
             '\n' + word + '\t' + transcription + '\t' + translation + '\t' + '<img src="' + picture + '">' + '\t' + '[sound:' + sound + ']')
 
@@ -102,7 +106,8 @@ def main():
     """
     Main entry point for the script.
     """
-    login(mail, passwd)
+    start()
+    login(Variables.mail, Variables.password)
     create_wordslist()
     for word in words:
         print word
